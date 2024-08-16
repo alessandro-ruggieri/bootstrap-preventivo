42a397bbb9570b0071e82759cf4a770d96a5c738
alessandro-ruggieri <alessandro.ruggieri95.ar@gmail.com>
2024-08-17 00:26:18 +0200

    Code cleaned and commented

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ formSelectElement.innerHTML += workTypeOptions
 
 //Assign the form to a variable
 const formElement = document.getElementById('form')
-console.log(formElement)
 
 //Fixed Hours and salary variable and assign the totalPrice area in the HTML page to a variable
 const hours = 10
@@ -28,7 +27,41 @@ const analysisSalary = 33.60
 let total
 let totalPrice = document.getElementById('totalPrice')
 
-//Function to calculate total price of the work
+//Function to validate form contents
+function validateForm() {
+   const formValidation = document.querySelectorAll(".to-validate")
+   let formIsValid = true
+   
+   formValidation.forEach(element => {
+      if (element.type === 'checkbox') { // Validate checkbox
+         if (!element.checked) {
+            element.classList.add('is-invalid');
+            formIsValid = false
+         } else {
+            element.classList.remove('is-invalid');
+         }
+      } else if (element.type === 'email') {
+         const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/  // RegExp for validating email pattern
+
+         if (element.value.trim() === "" || !emailPattern.test(element.value)) { // Validate mail. Only blank space are not allowed
+            element.classList.add('is-invalid');
+            formIsValid = false
+         } else {
+            element.classList.remove('is-invalid');
+         }
+      } else {
+         if (element.value.trim() === "") { // Validate text-input. Only blank space are not allowed
+            element.classList.add('is-invalid');
+            formIsValid = false
+         } else {
+            element.classList.remove('is-invalid');
+         }
+      }
+   })
+   return formIsValid // Return "true" if all the form fields are valid
+}
+
+//Function to calculate total price of the work with two decimals
 function calculateTotal() {
    if (formSelectElement.value === '0') {
       total = parseFloat(hours * backendSalary).toFixed(2)
@@ -37,7 +70,6 @@ function calculateTotal() {
    } else if (formSelectElement.value === '2') {
       total = parseFloat(hours * analysisSalary).toFixed(2)
    }
-   console.log(total)
    return total
 }
 
@@ -46,17 +78,16 @@ function applyCoupon() {
    const validCoupons = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24']
    const couponElement = document.getElementById('codicePromo')
    if (couponElement.value === "") {
-      console.log("Nessun coupon inserito")
+      //console.log("Nessun coupon inserito")
       couponElement.classList.remove('is-invalid')
       couponElement.classList.remove('is-valid')
    } else if (validCoupons.includes(couponElement.value)) {
-      console.log('Coupon attivato')
+      //console.log('Coupon attivato')
       couponElement.classList.remove('is-invalid')
       couponElement.classList.add('is-valid')
       total = parseFloat(total * 0.75).toFixed(2)
-      console.log(total)
    } else {
-      console.log('Coupon non valido')
+      //console.log('Coupon non valido')
       couponElement.classList.remove('is-valid')
       couponElement.classList.add('is-invalid')
       return total
@@ -66,44 +97,12 @@ function applyCoupon() {
 //Detect click on submit button from the user
 formElement.addEventListener('submit', function(event) {
    event.preventDefault()
-   console.log('Form Inviato')
+   //Reset price area in the DOM to avoid price concatenation
    totalPrice.innerHTML = `
    <h5>Prezzo Finale</h5>
    `
-   const formValidation = document.querySelectorAll(".to-validate")
-   console.log(formValidation)
-   let formIsValid = true
-   
-   formValidation.forEach(element => {
-      if (element.type === 'checkbox') {
-         if (!element.checked) {
-            element.classList.add('is-invalid');
-            formIsValid = false
-         } else {
-            element.classList.remove('is-invalid');
-         }
-      } else if (element.type === 'email') {
-         // Controllo se il campo email è vuoto o se il formato non è valido
-         const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/  // Espressione regolare per email
-
-         if (element.value.trim() === "" || !emailPattern.test(element.value)) {
-            element.classList.add('is-invalid');
-            formIsValid = false
-         } else {
-            element.classList.remove('is-invalid');
-         }
-      } else {
-         // Per tutti gli altri campi, verifica che non siano vuoti
-         if (element.value.trim() === "") {
-            element.classList.add('is-invalid');
-            formIsValid = false
-         } else {
-            element.classList.remove('is-invalid');
-         }
-      }
-   })
-
-   if (!formIsValid) {
+   //Call the validateForm function and if formIsValid is "false" return without continue with the calculation
+   if (!validateForm()) {
       return;
    }
 
@@ -112,7 +111,6 @@ formElement.addEventListener('submit', function(event) {
    
    // Split total in array so it can be printed in the DOM with separated integers and decimal parts
    const totalArray = total.split(".")
-   console.log(totalArray)
    totalPrice.innerHTML = `
    <h5>Prezzo Finale</h5>
    <span class="text-dark fw-bolder fs-4">€${totalArray[0]}</span><span class="text-muted fw-lighter">,${totalArray[1]}</span>
