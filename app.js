50117834265b8eda7cfc7cf12c7d3c446416550c
alessandro-ruggieri <alessandro.ruggieri95.ar@gmail.com>
2024-08-16 17:44:04 +0200

    CHANGE: Created functions for total price calculation and for apply coupon

--- app.js
+++ app.js
@@ -16,10 +16,11 @@ for (let i = 0; i < workType.length; i++) {
 //Inject the options in the select HTML
 formSelectElement.innerHTML += workTypeOptions
 
+//Assign the form to a variable
 const formElement = document.getElementById('form')
 console.log(formElement)
 
-//Hours and salary variable
+//Fixed Hours and salary variable and assign the totalPrice area in the HTML page to a variable
 const hours = 10
 const backendSalary = 20.50
 const frontendSalary = 15.30
@@ -27,6 +28,42 @@ const analysisSalary = 33.60
 let total
 let totalPrice = document.getElementById('totalPrice')
 
+//Function to calculate total price of the work
+function calculateTotal() {
+   if (formSelectElement.value === '0') {
+      total = parseFloat(hours * backendSalary).toFixed(2)
+   } else if (formSelectElement.value === '1') {
+      total = parseFloat(hours * frontendSalary).toFixed(2)
+   } else if (formSelectElement.value === '2') {
+      total = parseFloat(hours * analysisSalary).toFixed(2)
+   }
+   console.log(total)
+   return total
+}
+
+//Function to verify if user had used a valid coupon
+function applyCoupon() {
+   const validCoupons = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24']
+   const couponElement = document.getElementById('codicePromo')
+   if (couponElement.value === "") {
+      console.log("Nessun coupon inserito")
+      couponElement.classList.remove('is-invalid')
+      couponElement.classList.remove('is-valid')
+   } else if (validCoupons.includes(couponElement.value)) {
+      console.log('Coupon attivato')
+      couponElement.classList.remove('is-invalid')
+      couponElement.classList.add('is-valid')
+      total = parseFloat(total * 0.75).toFixed(2)
+      console.log(total)
+   } else {
+      console.log('Coupon non valido')
+      couponElement.classList.remove('is-valid')
+      couponElement.classList.add('is-invalid')
+      return total
+   }
+}
+
+//Detect click on submit button from the user
 formElement.addEventListener('submit', function(event) {
    event.preventDefault()
    console.log('Form Inviato')
@@ -70,33 +107,10 @@ formElement.addEventListener('submit', function(event) {
       return;
    }
 
-   if (formSelectElement.value === '0') {
-      total = parseFloat(hours * backendSalary).toFixed(2)
-   } else if (formSelectElement.value === '1') {
-      total = parseFloat(hours * frontendSalary).toFixed(2)
-   } else if (formSelectElement.value === '2') {
-      total = parseFloat(hours * analysisSalary).toFixed(2)
-   }
-   console.log(total)
-
-   const validCoupons = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24']
-   const couponElement = document.getElementById('codicePromo')
-   if (couponElement.value === "") {
-      console.log("Nessun coupon inserito")
-      couponElement.classList.remove('is-invalid')
-      couponElement.classList.remove('is-valid')
-   } else if (validCoupons.includes(couponElement.value)) {
-      console.log('Coupon attivato')
-      couponElement.classList.remove('is-invalid')
-      couponElement.classList.add('is-valid')
-      total = parseFloat(total * 0.75).toFixed(2)
-      console.log(total)
-   } else {
-      console.log('Coupon non valido')
-      couponElement.classList.remove('is-valid')
-      couponElement.classList.add('is-invalid')
-   }
+   calculateTotal()
+   applyCoupon()
    
+   // Split total in array so it can be printed in the DOM with separated integers and decimal parts
    const totalArray = total.split(".")
    console.log(totalArray)
    totalPrice.innerHTML = `
@@ -104,4 +118,3 @@ formElement.addEventListener('submit', function(event) {
    <span class="text-dark fw-bolder fs-4">€${totalArray[0]}</span><span class="text-muted fw-lighter">,${totalArray[1]}</span>
    `
 });
-
